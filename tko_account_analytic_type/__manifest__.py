b70a966c1d084f9d75b0d1703a0c080da8f330c5
Carlos Aguda <carlos.aguda@tkobr.com>
2017-07-28 12:02:11 +0100

    add modules from addons

---
+++ tko_account_analytic_type/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# © 2017 TKO <http://tko.tko-br.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Analytic Account Type',
+    'summary': '',
+    'description': 'This module adds type in analytic accounts',
+    'author': 'TKO',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'website': 'http://tko.tko-br.com',
+    'version': '10.0.0.0.0',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': ['analytic',
+                'account_analytic_default',
+                'account_analytic_plans',
+                'br_account',
+                'tko_br_account_account_analytic_plans',],
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'init_xml': [],
+    'update_xml': [],
+    'css': [],
+    'demo_xml': [],
+    'test': [],
+    'data': [
+        'views/analytic_account_view.xml',
+        'views/account_invoice_view.xml',
+             ],
+
+}
