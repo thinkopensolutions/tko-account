8d0e32df2131843463f20ab0e83b0a251a3fee7d
Devishree Brahmbhatt <brahm.devi@gmail.com>
2017-10-12 01:22:23 +0530

    [FIX]tko_account_parent_analytic: Exclude parent_hierarchy As per the urgent fix.

--- tko_account_parent_analytic/models/account_analytic_account.py
+++ tko_account_parent_analytic/models/account_analytic_account.py
@@ -35,7 +35,7 @@ class account_analytic_account(models.Model):
                 if aml.amount < 0.0:
                     debit += aml.amount
                 else:
-                    credit +=aml.amount
+                    credit += aml.amount
                 balance += aml.amount
             account.balance = balance
             account.credit = credit
@@ -43,7 +43,7 @@ class account_analytic_account(models.Model):
 
     parent_id = fields.Many2one('account.analytic.account', 'Parent Account', ondelete="set null")
     parent_hierarchy = fields.Many2one('account.analytic.account', 'Parent Hierarchy', ondelete="set null")
-    child_ids = fields.One2many('account.analytic.account', 'parent_hierarchy', 'Child Accounts')
+    child_ids = fields.One2many('account.analytic.account', 'parent_id', 'Child Accounts')
     child_complete_ids = fields.Many2many('account.analytic.account','analytic_account_rel', 'analytic_id','analytic_id_col',compute='_child_compute',
                                           string="Account Hierarchy")
     parent_left = fields.Integer('Left Parent', index=1)
@@ -52,7 +52,7 @@ class account_analytic_account(models.Model):
     debit = fields.Monetary(compute='compute_values', string='Debit')
     credit = fields.Monetary(compute='compute_values', string='Credit')
 
-    _parent_name = "parent_hierarchy"
+    _parent_name = "parent_id"
     _parent_store = True
     _parent_order = 'code, name'
     _order = 'parent_left'
@@ -69,16 +69,16 @@ class account_analytic_account(models.Model):
             res = []
             while cat:
                 res.append(cat.name)
-                cat = cat.parent_hierarchy
+                cat = cat.parent_id
             return res
         return [(cat.id, " / ".join(reversed(get_names(cat)))) for cat in self]
-
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
         result = super(account_analytic_account, self).read(fields, load='_classic_read')
         for rec in result:
-            if rec.get('parent_hierarchy'):
-                rec_res = self.browse(rec.get('parent_hierarchy')[0])
-                result[0].update({'parent_hierarchy': (rec.get('parent_hierarchy')[0], rec_res.name)})
+            if rec.get('parent_id'):
+                rec_res = self.browse(rec.get('parent_id')[0])
+                result[0].update({'parent_id': (rec.get('parent_id')[0], rec_res.name)})
         return result
+
