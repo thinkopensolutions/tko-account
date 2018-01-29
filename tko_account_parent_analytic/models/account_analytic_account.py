72ff5c9924cb45cc63fcc56657c89627f01787e9
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-01-29 19:03:33 -0200

    Update account_analytic_account.py

--- tko_account_parent_analytic/models/account_analytic_account.py
+++ tko_account_parent_analytic/models/account_analytic_account.py
@@ -33,18 +33,23 @@ class account_analytic_account(models.Model):
             search_domain.insert(0,('account_id','in',sub_accounts.ids))
             for aml in self.env['account.analytic.line'].search(search_domain):
                 if aml.amount < 0.0:
-                    debit= aml.amount
+                    debit += aml.amount
                 else:
-                    credit=aml.amount
-                balance= aml.amount
-            account.balance += balance
+                    credit += aml.amount
+                balance += aml.amount
+            account.balance = balance
             account.credit = credit
             account.debit = debit
 
     parent_id = fields.Many2one('account.analytic.account', 'Parent Account', ondelete="set null")
+    parent_hierarchy = fields.Many2one('account.analytic.account', 'Parent Hierarchy', ondelete="set null")
     child_ids = fields.One2many('account.analytic.account', 'parent_id', 'Child Accounts')
     child_complete_ids = fields.Many2many('account.analytic.account','analytic_account_rel', 'analytic_id','analytic_id_col',compute='_child_compute',
                                           string="Account Hierarchy")
+
+    balance = fields.Monetary(compute='_compute_debit_credit_balance', string='Balance')
+    debit = fields.Monetary(compute='_compute_debit_credit_balance', string='Debit')
+    credit = fields.Monetary(compute='_compute_debit_credit_balance', string='Credit')
     parent_left = fields.Integer('Left Parent', index=1)
     parent_right = fields.Integer('Right Parent', index=1)
     balance = fields.Monetary(compute='compute_values', string='Balance')
@@ -56,7 +61,55 @@ class account_analytic_account(models.Model):
     _parent_order = 'code, name'
     _order = 'parent_left'
 
+    @api.model
+    def _move_domain_get(self, domain=None):
+        domain = domain and safe_eval(str(domain)) or []
+        if self._context.get('from_date', False):
+            domain.append(('date', '>=', self._context['from_date']))
+        if self._context.get('to_date', False):
+            domain.append(('date', '<=', self._context['to_date']))
+        return domain
+
+
+    @api.multi
+    def _compute_debit_credit_balance(self):
+        analytic_line_obj = self.env['account.analytic.line']
+        default_domain = self._move_domain_get()
+        for account in self:
+            sub_accounts = self.with_context({'show_parent_account': True}).search([('id', 'child_of', [account.id])])
+            credit = 0.0
+            debit = 0.0
+            search_domain = default_domain[:]
+            search_domain.insert(0, ('account_id', 'in', sub_accounts.ids))
+            for aal in analytic_line_obj.search(search_domain):
+                if aal.amount < 0.0:
+                    debit += aal.amount
+                else:
+                    credit += aal.amount
+            account.balance = credit - debit
+            account.credit = credit
+            account.debit = abs(debit)
 
     @api.one
     def _child_compute(self):
         self.child_complete_ids = [(6, 0, [child.id for child in self.child_ids])]
+
+    @api.multi
+    def name_get(self):
+        def get_names(cat):
+            """ Return the list [parent.name, parent.parent_id.name, ...] """
+            res = []
+            while cat:
+                res.append(cat.name)
+                cat = cat.parent_id
+            return res
+        return [(cat.id, " / ".join(reversed(get_names(cat)))) for cat in self]
+
+    @api.multi
+    def read(self, fields=None, load='_classic_read'):
+        result = super(account_analytic_account, self).read(fields, load='_classic_read')
+        for rec in result:
+            if rec.get('parent_id'):
+                rec_res = self.browse(rec.get('parent_id')[0])
+                result[0].update({'parent_id': (rec.get('parent_id')[0], rec_res.name)})
+        return result
