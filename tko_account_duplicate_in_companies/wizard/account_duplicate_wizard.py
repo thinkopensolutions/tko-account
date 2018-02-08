cec4e4f97739b014ecbd0a440fcc5fe29f2d0721
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-02-08 15:03:32 -0200

    FIX default_get()

--- tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
+++ tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
@@ -11,7 +11,7 @@ class DuplicateAccounts(models.TransientModel):
     def default_get(self, field_list):
         res = super(DuplicateAccounts, self).default_get(field_list)
         res.update({
-            'company_id': [(6, 0, self.env['res.company'].sudo().search([]).ids )],
+            'company_ids': [(6, 0, self.env['res.company'].sudo().search([]).ids )],
         })
         return res
 
@@ -19,7 +19,7 @@ class DuplicateAccounts(models.TransientModel):
     def duplicate_accounts(self):
         self = self.sudo()
         active_ids = self._context.get('active_ids',[])
-        accounts = self.env['account.account'].sudo().browse('active_ids')
+        accounts = self.env['account.account'].sudo().browse(active_ids)
         for company in self.company_ids:
             accounts.copy(default={'company_id' : company.id})
         return True
