674312a3df11acc411e2b3d210a7bf085faf9bdf
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-02-08 15:26:50 -0200

    add code

--- tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
+++ tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
@@ -23,7 +23,7 @@ class DuplicateAccounts(models.TransientModel):
         new_accounts = []
         for company in self.company_ids:
             for account in accounts:
-                new_account = account.copy(default={'name': account.name, 'company_id': company.id})
+                new_account = account.copy(default={'name': account.name, 'code': account.code, 'company_id': company.id})
                 new_accounts.append(new_account.id)
         return {
             'name': _('Chart of Accounts'),
