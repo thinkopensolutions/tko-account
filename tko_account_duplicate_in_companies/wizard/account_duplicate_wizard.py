65e0a2e2ecc68a13123c83bc9139a61b1c120bd2
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-02-08 14:49:21 -0200

    import fields

--- tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
+++ tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api, _
+from odoo import models, fields, api, _
 
 class DuplicateAccounts(models.TransientModel):
     _name = 'duplicate.accounts'
