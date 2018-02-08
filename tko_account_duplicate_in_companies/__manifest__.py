f6211f5ff802d3664e8ed542cd500e1076d046e2
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-02-08 14:13:37 -0200

    add module tko_account_duplicate_in_companies

---
+++ tko_account_duplicate_in_companies/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# © 2017 TKO <http://tko.tko-br.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Duplicate Selected Accounts in all or selected companies',
+    'summary': '',
+    'description': 'Duplicate Accounts',
+    'author': 'TKO',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'website': 'http://tko.tko-br.com',
+    'version': '10.0.0.0.0',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': ['account'],
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'init_xml': [],
+    'update_xml': [],
+    'css': [],
+    'demo_xml': [],
+    'test': [],
+    'data': [
+        'wizard/account_duplicate_wizard_view.xml'
+             ],
+
+}
---
+++ tko_account_duplicate_in_companies/wizard/account_duplicate_wizard.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, _
+
+class DuplicateAccounts(models.TransientModel):
+    _name = 'duplicate.accounts'
+
+    company_ids = fields.Many2many('res.company','Company')
+
+    @api.model
+    def default_get(self, field_list):
+        res = super(DuplicateAccounts, self).default_get(field_list)
+        res.update({
+            'company_id': [(6, 0, self.env['res.company'].sudo().search([]) )],
+        })
+        return res
+
+    @api.one
+    def duplicate_accounts(self):
+        self = self.sudo()
+        active_ids = self._context.get('active_ids',[])
+        accounts = self.env['account.account'].sudo().browse('active_ids')
+        for company in self.company_ids:
+            accounts.copy(default={'company_id' : company.id})
+        return True
