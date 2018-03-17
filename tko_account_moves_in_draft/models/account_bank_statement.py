8c9a5fc32298678a393dc936b96a34293885355e
yk2kus <yogeshkushwaha4@gmail.com>
2018-03-17 19:48:04 -0300

    filter account moves line in draft stage on reconciliation widget

---
+++ tko_account_moves_in_draft/models/account_bank_statement.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, _
+from odoo.exceptions import UserError, RedirectWarning, ValidationError
+from odoo.osv import expression
+
+class AccountBankStatementLine(models.Model):
+    _inherit = "account.bank.statement.line"
+    _description = "Bank Statement Line"
+
+    ####################################################
+    # Show only Posted Account Move Lines
+    # in Reconciliation Widget
+    ####################################################
+
+    def get_move_lines_for_reconciliation(self, excluded_ids=None, str=False, offset=0, limit=None,
+                                          additional_domain=None, overlook_partner=False):
+        """ Return account.move.line records which can be used for bank statement reconciliation.
+
+            :param excluded_ids:
+            :param str:
+            :param offset:
+            :param limit:
+            :param additional_domain:
+            :param overlook_partner:
+        """
+        if additional_domain is None:
+            additional_domain = []
+        else:
+            additional_domain = expression.normalize_domain(additional_domain)
+        additional_domain = expression.AND([additional_domain, [('move_id.state', '=', 'posted')]])
+
+        return super(AccountBankStatementLine, self).get_move_lines_for_reconciliation(excluded_ids=excluded_ids, str=str,
+                                                                                offset=offset, limit=limit,
+                                                                                additional_domain=additional_domain,
+                                                                                overlook_partner=overlook_partner)
